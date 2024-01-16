0fa5a1f197e087f013ce9ef34cffd9535ad0dc0b
Nathan Palmer <153049405+palmer061397@users.noreply.github.com>
2024-01-16 17:08:17 -0600

    Update main.py
    Improved readability of code,
    Added docstrings
    Reduced repetitive plant_file.write with writlines.

--- main.py
+++ main.py
@@ -1,79 +1,122 @@
 import json
+import traceback
+import logging
 import plantuml
 
+# Configuring logging
+logging.basicConfig(filename='main.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+
+
+#DIR of json files
+JSON_DIR = "./JSON_Responses"
+
+#json filenames
+json_files = ["/profile_service_response.json", #index 0
+			  "/posts_management_service_response.json", #index 1
+			  "/moderation_service_response.json"] #index 2
+					 
+# List of attribute names equals (x)					 
+username_data = [" : biography = ", #index 0
+        		 " : hyperlink = ", #index 1
+        		 " : is_reported = ", #index 2
+        		 " : is_shadowbanned = ", #index 3
+        		 " : number_of_followers = ", #index 4
+        		 " : number_of_posts = ", #index 5
+        		 " : number_of_following = ", #index 6:
+        		 " : name = "] #index 7
+
+# List of attribute names      						 
+info_list = ["username", #index 0
+        	 "biography", #index 1
+        	 "hyperlink", #index 2
+        	 "is_reported", #index 3
+        	 "is_shadowbanned", #index 4
+        	 "number_of_followers", #index 5
+        	 "number_of_posts", #index 6
+        	 "number_of_following", #index 7
+        	 "name"] #index 8
 
 def load_data():
-    with open("./JSON_Responses/profile_service_response.json", encoding="utf-8") as profile_file:
-        profile_json = json.load(profile_file)
-
-    with open("./JSON_Responses/posts_management_service_response.json", encoding="utf-8") as posts_file:
-        posts_json = json.load(posts_file)
-
-    with open("./JSON_Responses/moderation_service_response.json", encoding="utf-8") as moderation_file:
-        moderation_json = json.load(moderation_file)
-
+    """ Opens json files, assigns loaded data from file to variable
+        , then returns all variables storing file data"""
+    try:
+    	with open(JSON_DIR + json_files[0], encoding="utf-8") as profile_file:
+        	profile_json = json.load(profile_file)
+    except FileNotFoundError:
+    	logging.exception(traceback.print_exc())
+    	
+    try:
+    	with open(JSON_DIR + json_files[1], encoding="utf-8") as posts_file:
+        	posts_json = json.load(posts_file)
+    except FileNotFoundError:
+    	logging.exception(traceback.print_exc())
+    	
+    try:
+    	with open(JSON_DIR + json_files[2], encoding="utf-8") as moderation_file:
+        	moderation_json = json.load(moderation_file)
+    except FileNotFoundError:
+    	logging.exception(traceback.print_exc())
     return (profile_json, posts_json, moderation_json)
 
-
 def write_plantuml_file(profile_data, posts_data, moderation_data):
-    with open("plantuml.txt", "w", encoding="utf-8") as pf:
-        # object nameOfNode
-        pf.write("@startuml \n")
-        pf.write("object " + profile_data["username"] + "\n")
-        # nameOfNode : nameOfAttribute = attributeValue
-        pf.write(profile_data["username"] +
-                 " : name = " + profile_data["name"] + "\n")
-        pf.write(profile_data["username"] + " : biography = " +
-                 profile_data["biography"] + "\n")
-        pf.write(profile_data["username"] + " : hyperlink = " +
-                 profile_data["hyperlink"] + "\n")
-        pf.write(profile_data["username"] + " : is_reported = " +
-                 str(profile_data["is_reported"]) + "\n")
-        pf.write(profile_data["username"] + " : is_shadowbanned = " +
-                 str(profile_data["is_shadowbanned"]) + "\n")
-        pf.write(profile_data["username"] + " : number_of_followers = " +
-                 str(profile_data["number_of_followers"]) + "\n")
-        pf.write(profile_data["username"] + " : number_of_posts = " +
-                 str(profile_data["number_of_posts"]) + "\n")
-        pf.write(profile_data["username"] + " : number_of_following = " +
-                 str(profile_data["number_of_following"]) + "\n")
+    with open("./plantuml.txt", "w", encoding="utf-8") as plant_file:
+      
+        # plantuml object nameOfNode
+        plant_file.write("@startuml \n")
+        plant_file.write("object " + profile_data[info_list[0]] + "\n")
+        
+         # nameOfNode : nameOfAttribute = attributeValue
+        plant_file.writelines(profile_data[info_list[0]] + username_data[7] + profile_data[info_list[8]] + "\n" +
+        					  profile_data[info_list[0]] + username_data[0] + profile_data[info_list[1]] + "\n" +
+        					  profile_data[info_list[0]] + username_data[1] + profile_data[info_list[2]] + "\n" +
+        					  profile_data[info_list[0]] + username_data[2] + str(profile_data[info_list[3]]) + "\n" +
+        					  profile_data[info_list[0]] + username_data[3] + str(profile_data[info_list[4]]) + "\n" +
+        					  profile_data[info_list[0]] + username_data[4] + str(profile_data[info_list[5]]) + "\n" +
+        					  profile_data[info_list[0]] + username_data[5] + str(profile_data[info_list[6]]) + "\n" +
+        					  profile_data[info_list[0]] + username_data[6] + str(profile_data[info_list[7]]) + "\n"
+        					  )
 
         for post in posts_data:
-            pf.write("object post_" + str(post["post_id"]) + "\n")
-            pf.write("post_" + str(post["post_id"]) +
-                     " : post_caption = " + post["post_caption"] + "\n")
-            pf.write("post_" + str(post["post_id"]) +
-                     " : images = " + str(post["images"]) + "\n")
-            pf.write("post_" + str(post["post_id"]) +
-                     " : comments = " + str(post["comments"]) + "\n")
-            pf.write("post_" + str(post["post_id"]) +
-                     " : hashtags = " + str(post["hashtags"]) + "\n")
-            pf.write("post_" + str(post["post_id"]) +
-                     " : date_published = " + post["date_published"] + "\n")
-            # nameOfNodeOne -> nameOfNodeTwo
-            pf.write(profile_data["username"] + " -down-> " +
+            try:
+            	plant_file.write("object post_" + str(post["post_id"]) + "\n")
+            	plant_file.write("post_" + str(post["post_id"]) + " : post_caption = " + post["post_caption"] + "\n")
+            	plant_file.write("post_" + str(post["post_id"]) + " : images = " + str(post["images"]) + "\n")
+            	plant_file.write("post_" + str(post["post_id"]) + " : comments = " + str(post["comments"]) + "\n")
+            	plant_file.write("post_" + str(post["post_id"]) + " : hashtags = " + str(post["hashtags"]) + "\n")
+            	plant_file.write("post_" + str(post["post_id"]) + " : date_published = " + post["date_published"] + "\n")
+            except Exception as e:
+            	logging.critical(e)
+          
+            # nameOfNodeOne -> nameOfNodeTwo : plantuml formatted
+            plant_file.write(profile_data["username"] + " -down-> " +
                      "post_" + str(post["post_id"]) + "\n")
 
         for moderated_post in moderation_data:
-            pf.write("post_" + str(moderated_post["post_id"]) +
+            plant_file.write("post_" + str(moderated_post["post_id"]) +
                      " : is_reported = " + str(moderated_post["is_reported"]) + "\n")
-            pf.write("post_" + str(moderated_post["post_id"]) +
+            plant_file.write("post_" + str(moderated_post["post_id"]) +
                      " : is_manual = " + str(moderated_post["is_manual"]) + "\n")
-            pf.write("post_" + str(moderated_post["post_id"]) +
+            plant_file.write("post_" + str(moderated_post["post_id"]) +
                      " : reason = " + moderated_post["reason"] + "\n")
 
             if "reported_by" in moderated_post:
-                pf.write(
-                    "post_" + str(moderated_post["post_id"]) + " : reported_by = " + moderated_post["reported_by"] + "\n")
-        pf.write("@enduml \n")
+                plant_file.write("post_" + str(moderated_post["post_id"]) + " : reported_by = " + moderated_post["reported_by"] + "\n")
+        plant_file.write("@enduml \n")
 
 
 def create_plantuml_image():
-    plantuml.PlantUML("http://www.plantuml.com/plantuml/img/").processes_file(
-        "plantuml.txt", outfile=None, errorfile=None)
-
+    """ (create_plantuml_function) reads plantuml.txt
+         data and sends it to website to create and capture image"""
+    try:
+    	plantuml.PlantUML("http://www.plantuml.com/plantuml/img/").processes_file("./plantuml.txt", outfile=None, errorfile=None)
+    except Exception as e:
+    	logging.critical(e)
 
 if __name__ == '__main__':
-    profile, posts, moderation = load_data()
-    write_plantuml_file(profile, posts, moderation)
-    create_plantuml_image()
+    try:
+    	profile, posts, moderation = load_data()
+    	write_plantuml_file(profile, posts, moderation)
+    	create_plantuml_image()
+    except Exception as e:
+    	logging.exception(e)
+    
