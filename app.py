bf4497b86145427656b368fa47cd7bb3a25a87e9
Nathan Palmer <153049405+palmer061397@users.noreply.github.com>
2024-01-15 21:57:59 -0600

    Update app.py
    Added doctstrings and made changes to code for better readability. 
    Added exception catching and logging.

--- app.py
+++ app.py
@@ -1,36 +1,57 @@
-import json
+
 import plantuml
 import requests
+import logging
+import traceback
 
+#configuring logging module for logs
+logging.basicConfig(filename='app.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
 def load_data():
+    """ Opens json files, assigns loaded data from file to variable
+        , then returns variables storing file data"""
     response = requests.get(
         "https://randomuser.me/api/?format=json", timeout=5000)
-    if response.status_code == 200:
-        return response.json()["results"][0]
+    if response.status_code >= 200 & response.status_code <= 299:
+    	return response.json()["results"][0]
     else:
-        print("Data did not load correctly")
-        return None
+    	return logging.exception("'response' in load_data() function: Returned Status_code = {0}".format(response.status_code))
+    	
 
 
 def write_plantuml_file(res):
-    nodeName = res["name"]["first"] + "_" + res["name"]["last"]
-    with open("plantuml.txt", "w", encoding="utf-8") as pf:
-        pf.write("@startuml \n")
-        pf.write("object " + nodeName + " \n")
-        pf.write(nodeName + " : email = " + res["email"] + "\n")
-        pf.write(nodeName + " : phone = " + res["phone"] + "\n")
-        pf.write(nodeName + " : age = " + str(res["dob"]["age"]) + "\n")
-        pf.write("@enduml \n")
+    """opens plantuml.txt and writes data to file"""
+    
+    # Stores index of firstname and index lastname to write to file
+    # The format is required for plantuml
+    node_name = res["name"]["first"] + "_" + res["name"]["last"]
+    # Stores nodeName and concats index of email, phone, and age
+    # The format is required for plantuml
+    json_options = [node_name + " : email =" + res["email"] + "\n",
+    						 node_name + " : phone = " + res["phone"] + "\n",
+    						 node_name + " : age = " + str(res["dob"]["age"]) + "\n"]
+    try:
+    	with open("./PlantUML/plantuml.txt", "w", encoding="utf-8") as plant_file:
+        	plant_file.write("@startuml \n")
+        	plant_file.write("object " + node_name + " \n")
+        	for i in range(3):
+        		plant_file.write(json_options[i])
+        	plant_file.write("@enduml \n")
+    except FileNotFoundError:
+    	# writes traceback details to file "app.log"
+    	logging.exception(traceback.print_exc())
 
 
 def create_plantuml_image():
+    """ (create_plantuml_function) reads plantuml.txt
+         data and sends it to website to create and capture image"""
     plantuml.PlantUML("http://www.plantuml.com/plantuml/img/").processes_file(
-        "plantuml.txt", outfile=None, errorfile=None)
+        "./PlantUML/plantuml.txt", outfile=None, errorfile=None)
 
 
 if __name__ == '__main__':
     response_json = load_data()
-    if response_json != None:
+    if response_json is not None:
         write_plantuml_file(response_json)
         create_plantuml_image()
+        
