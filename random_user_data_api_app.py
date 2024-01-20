cdcc3633b5730eb1468514c11234704fed47c68f
Nathan Palmer <153049405+palmer061397@users.noreply.github.com>
2024-01-20 03:03:38 -0600

    Update random_user_data_api_app.py
    Removed unnecessary whitespace

--- random_user_data_api_app.py
+++ random_user_data_api_app.py
@@ -46,8 +46,8 @@ def write_plantuml_file(res):
     """opens plantuml.txt and writes data to file"""
     
     node_data_choices = {"email" : " : email = ", #index 0
-    										"phone" : " : phone = ", #index 1
-    										"age" : " : age = "} #index 2
+    					 "phone" : " : phone = ", #index 1
+    					 "age" : " : age = "} #index 2
     										
     # Stores index of firstname and index lastname to write to file
     node_name = res["name"]["first"] + "_" + res["name"]["last"]
@@ -55,8 +55,8 @@ def write_plantuml_file(res):
     # Stores nodeName and concats index of email, phone, and age
     # The format is required for plantuml
     json_options = [node_name + node_data_choices["email"] + res[Profile_API_Attributes.email] + "\n",
-    						    node_name + node_data_choices["phone"] + res[Profile_API_Attributes.phone] + "\n",
-    						    node_name + node_data_choices["age"] + str(res[Profile_API_Attributes.dob][Profile_API_Attributes.age]) + "\n"]
+    				node_name + node_data_choices["phone"] + res[Profile_API_Attributes.phone] + "\n",
+    				node_name + node_data_choices["age"] + str(res[Profile_API_Attributes.dob][Profile_API_Attributes.age]) + "\n"]
     try:
     	with open("./Docs/PlantUML_Txt/plantuml.txt", "w", encoding="utf-8") as plant_txt:
         	plant_txt.write(UMLCmds.start_uml)
