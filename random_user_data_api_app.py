887139a08f5d9cea0752f1f759cc4b01251961ec
Nathan Palmer <153049405+palmer061397@users.noreply.github.com>
2024-01-20 23:08:13 -0600

    Update random_user_data_api_app.py
    Additional logging implemented

--- random_user_data_api_app.py
+++ random_user_data_api_app.py
@@ -5,12 +5,17 @@ import plantuml
 import requests
 import sys
 
-# Insers custom module from different directory
-sys.path.insert(0, './Docs/Custom_Modules')
-from JSONAttributeModule import *
-
 # Configuring logging module for logs
 logging.basicConfig(filename='./Docs/Log_Files/random_user_data_api_log_file.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+
+try: # Insers custom module from different directory
+    sys.path.insert(0, './Docs/Custom_Modules')
+    from JSONAttributeModule import *
+    logging.info("Custom Module 'JSONAttributeModule' imported successfully.")
+except (ImportError, IOError) as e:
+    logging.critical(e)
+
+
 
 # API Address
 api_address = "https://randomuser.me/api/?format=json"
@@ -23,12 +28,12 @@ def load_data():
         # logs exception to specified log file from above
         logging.critical(e)
 
-    returned_status_code = response.ok
-    if returned_status_code is True:
+    
+    if response.ok:
         logging.info("Successful: web returned status code {0}".format(response.status_code))
         return response.json()["results"][0]
     else:
-        logging.exception("'reponse' in function load_data() bad status code")
+        logging.error("'reponse' call returned status code: status code = {0}".format(response.status_code))
 
 
 def write_plantuml_file(res):
@@ -44,8 +49,8 @@ def write_plantuml_file(res):
     # Stores nodeName and concats index of email, phone, and age
     # The format is required for plantuml
     json_options = [node_name + node_data_choices["email"] + res[Profile_API_Attributes.email] + "\n",
-    						    node_name + node_data_choices["phone"] + res[Profile_API_Attributes.phone] + "\n",
-    						    node_name + node_data_choices["age"] + str(res[Profile_API_Attributes.dob][Profile_API_Attributes.age]) + "\n"]
+    				node_name + node_data_choices["phone"] + res[Profile_API_Attributes.phone] + "\n",
+    				node_name + node_data_choices["age"] + str(res[Profile_API_Attributes.dob][Profile_API_Attributes.age]) + "\n"]
     
     try:
         with open("./Docs/PlantUML_Txt/plantuml.txt", "w", encoding="utf-8") as plant_txt:
@@ -64,12 +69,15 @@ def create_plantuml_image():
     """ reads plantuml.txt data and sends to webiste to generate palntuml diagram in png format"""
     try:
         plantuml.PlantUML("http://www.plantuml.com/plantuml/img/").processes_file("./Docs/PlantUML_Txt/plantuml.txt", outfile="./PlantUML_Images/API_Profile_Diagram.png", errorfile=None)
+        logging.info("Connection to plantuml.com/plantuml/img/ : successful")
+        logging.info("Processing file plantuml.txt successful.")
     except (FileNotFoundError, plantuml.PlantUMLConnectionError, plantuml.PlantUMLError, plantuml.PlantUMLHTTPError) as e:
         logging.critical(e)
 
 if __name__ == '__main__':
     # assigns 'load_data()'' function to variable 'response_json''
     response_json = load_data()
+    # checks that response_json variable contains data, if None raise exception and log to log file
     if response_json is not None:
         write_plantuml_file(response_json)
         create_plantuml_image()
